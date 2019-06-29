60c7606ae04ac54a18ba79d429f02b94414f66f6
NotGoldun <domeniko.sutlovic@gmail.com>
2019-06-29 13:33:21 +0200

    template exercise

---
+++ 29.06.19/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename T>
+T Sum(T a, T b)
+{
+	return a + b;
+}
+template <class _Ty>
+class MyArray
+{
+public:
+	_Ty table[];
+
+	void push_back(_Ty element);
+};
+
+
+
+int main()
+{
+	int sumNumber = Sum(5, 7);
+	float floatingSum = Sum(3.4f, 7.7f);
+	std::cout << "Zbroj intova: " << sumNumber << std::endl;
+	std::cout << "Zbroj floatova: " << floatingSum << std::endl;
+
+	std::string first = "Maja";
+	std::string second = "dev";
+	std::string stringSum = Sum(first, second);
+
+	std::cout << "Zbroj stringova: " << stringSum << std::endl;
+
+	std::vector<int> vectorTemplate;
+
+	std::cin.get();
+}
