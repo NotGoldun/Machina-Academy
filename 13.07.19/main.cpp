8edc2d19f8e46f5a50defeda74416beb4f934de3
NotGoldun <domeniko.sutlovic@gmail.com>
2019-07-13 15:25:16 +0200

    STD containers

--- 13.07.19/main.cpp
+++ 13.07.19/main.cpp
@@ -1,37 +1,96 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
 
-int sumDigits(int number)
-{
-	int sum = 0;
-	while (number != 0)
-	{
-		sum  += number % 10;
-		number /= 10;
-	}
-	return sum;
-}
 
 int main()
 {
-	int firstNumber, secondNumber;
-	std::cout << "Upisite prvi broj: " << std::endl;
-	std::cin >> firstNumber;
-	std::cin.ignore(1000, '\n');
-	std::cin.clear();
-	std::cout << "Upisite drugi broj: " << std::endl;
-	std::cin >> secondNumber;
-	std::cin.ignore(1000, '\n');
-	std::cin.clear();
+	std::vector<int> myVector;
 
-	if (sumDigits(firstNumber) == sumDigits(secondNumber))
+	std::pair<std::string, std::string> myPair;
+	myPair.first = "Arton";
+	myPair.second = "Glocke";
+
+	std::pair<std::string, std::string> secondPair = std::make_pair<std::string, std::string> ("Aron" , "Thompson");
+
+	std::map<std::string, std::string> myMap;
+	myMap.insert(myPair);
+	myMap.insert(secondPair);
+	myMap.insert(std::make_pair<std::string, std::string>("Maja", "Stipetic"));
+	myMap.insert(std::make_pair<std::string, std::string>("Domeniko", "Sutlovic"));
+
+	for (auto it : myMap)
 	{
-		std::cout << "Brojevi " << firstNumber << " i " << secondNumber << " imaju isti zbroj znamenki i on iznosi: " << sumDigits(firstNumber) << std::endl;
+		std::cout << it.first << ", ";
+		std::cout << it.second << std::endl;
 	}
-	else
+
+	std::cout << myMap["Maja"] << std::endl;
+
+	myMap.insert(std::make_pair<std::string, std::string>("Patricija", "Zubalic"));
+	myMap.insert(std::make_pair<std::string, std::string>("Patricija", "B"));
+	
+	std::queue<std::string> myQueue;
+	myQueue.push("Arton");
+	myQueue.push("Maja");
+	myQueue.push("Domeniko");
+	myQueue.push("Aron");
+	myQueue.push("Iggy");
+	myQueue.push("Kristijan");
+	myQueue.push("Adnan");
+	myQueue.push("Aleksandar");
+
+	while (!myQueue.empty())
 	{
-		std::cout << "Brojevi " << firstNumber << " i " << secondNumber << " nemaju isti zbroj znamenki." << std::endl;
+		std::cout << myQueue.front() << std::endl;
+		myQueue.pop();
+	}
+
+	std::stack<std::string> myStack;
+	myStack.push("Arton");
+	myStack.push("Maja");
+	myStack.push("Domeniko");
+	myStack.push("Aron");
+	myStack.push("Iggy");
+	myStack.push("Kristijan");
+	myStack.push("Adnan");
+	myStack.push("Aleksandar");
+
+	while (!myStack.empty())
+	{
+		std::cout << myStack.top() << std::endl;
+		myStack.pop();
+	}
+
+	std::deque<std::string> myDeque;
+	myDeque.push_back("Arton");
+	myDeque.push_back("Maja");
+	myDeque.push_back("Domeniko");
+	myDeque.push_back("Aron");
+	myDeque.push_back("Iggy");
+	myDeque.push_back("Kristijan");
+	myDeque.push_back("Adnan");
+	myDeque.push_back("Aleksandar");
+
+	while (!myDeque.empty())
+	{
+		std::cout << myDeque.back() << std::endl;
+		myDeque.pop_back();
+
+		std::cout << myDeque.front() << std::endl;
+		myDeque.pop_front();
 
 	}
+
+
+
+
+
+
 
 	std::cin.get();
 }
