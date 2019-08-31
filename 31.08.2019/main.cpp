58d43207c4c5302ac6c8d02bc708c14d7acadb89
NotGoldun <domeniko.sutlovic@gmail.com>
2019-08-31 13:48:04 +0200

    Strong numbers

--- 31.08.2019/main.cpp
+++ 31.08.2019/main.cpp
@@ -1,89 +1,42 @@
 #include <iostream>
 
-int main() 
+int factorialSUM(int number)
 {
-	//int firstNumber, secondNumber;
-	//
-	//std::cout << "Upisite dva prirodna broja: " << std::endl;
-	//std::cout << "Prvi broj: ";
-	//std::cin >> firstNumber;
-	//std::cin.ignore(1000, '\n');
-	//std::cin.clear();
-	//std::cout << std::endl;
-	//std::cout << "Drugi broj: ";
-	//std::cin >> secondNumber;
-	//std::cin.ignore(1000, '\n');
-	//std::cin.clear();
-	//std::cout << std::endl;
-
-	//int MAX;
-	//int divisionNumber = 2;
-
-	//if (firstNumber > secondNumber)
-	//{
-	//	MAX = secondNumber;
-	//}
-	//else
-	//{
-	//	MAX = firstNumber;
-	//}
-
-	//while (divisionNumber <= MAX)
-	//{
-	//	if (firstNumber % divisionNumber == 0 && secondNumber % divisionNumber == 0)
-	//	{
-	//		std::cout << "Brojevi " << firstNumber << " i " << secondNumber << " nisu relativno prosti" << std::endl;
-	//		std::cin.get();
-	//		return 0;
-	//	}
-	//	else
-	//	{
-	//		divisionNumber++;
-	//	}
-	//}
-
-	//
-	//std::cout << "Brojevi " << firstNumber << " i " << secondNumber << " su relativno prosti" << std::endl;
-
-	//int thirdNumber;
-	//int secondDivisionNumber = 2;
-
-	//std::cout << "Upisite prirodni broj: ";
-	//std::cin >> thirdNumber;
-	//std::cin.ignore(1000, '\n');
-	//std::cin.clear();
-	//std::cout << std::endl;
-	//
-	//while (secondDivisionNumber < thirdNumber)
-	//{
-	//	if (thirdNumber % secondDivisionNumber == 0)
-	//	{
-	//		std::cout << "Broj " << thirdNumber << " nije prost broj" << std::endl;
-	//		std::cin.get();
-	//		return 0;
-	//	}
-	//	else
-	//	{
-	//		secondDivisionNumber++;
-	//	}
-	//}
-	//std::cout << "Broj " << thirdNumber << " je prost broj" << std::endl;
-
-	int fourthNumber;
-	std::cout << "Upisite prirodni broj: ";
-	std::cin >> fourthNumber;
-	std::cin.ignore(1000, '\n');
-	std::cin.clear();
-	std::cout << std::endl;
-
-	if (fourthNumber % 2 == 0 || fourthNumber % 3 == 0 || fourthNumber % 5 == 0 || fourthNumber % 7 == 0)
+	int SUM = 0;
+	int factorialNumber;
+	while (number != 0)
 	{
-		std::cout << "Broj " << fourthNumber << " nije prost broj" << std::endl;
+		factorialNumber = number % 10;
+		int factorial = 1;
+		while (factorialNumber > 0)
+		{
+			factorial *= factorialNumber;
+			factorialNumber--;
+		}
+		SUM += factorial;
+		number /= 10;
 	}
-	else
-	{
-		std::cout << "Broj " << fourthNumber << " je prost broj" << std::endl;
-	}
+	return SUM;
+}
 
+int main()
+{
+	int number = 2;
+	//std::cout << "Upisite broj: ";
+	//std::cin >> number;
+	//std::cin.ignore(1000, '\n');
+	//std::cin.clear();
+	while (number < 100000)
+	{
+		if (factorialSUM(number) == number)
+		{
+			std::cout << "Broj " << number << " je jak broj" << std::endl;
+		}
+		//else
+		//{
+		//	std::cout << "Broj " << number << " nije jak broj" << std::endl;
+		//}
+		number++;
+	}
 	std::cin.get();
 }
