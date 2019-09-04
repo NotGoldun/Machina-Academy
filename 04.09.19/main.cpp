043a537649387ea576abad47e5fdcfb501e0c0b2
NotGoldun <domeniko.sutlovic@gmail.com>
2019-09-04 21:03:37 +0200

    overloading operators

--- 04.09.19/main.cpp
+++ 04.09.19/main.cpp
@@ -1,20 +1,60 @@
 #include <iostream>
+#include <string>
 
-int main()
+class byte
 {
-	uint32_t arr[] = { 10,17,13,15 };
-	for (int i = 0; arr[i] != '\0' ; i++)
+public:
+
+	byte operator+(byte other)
 	{
-		if (arr[i] % 2 != 0 && arr[i] % 3 != 0 && arr[i] % 5 != 0 && arr[i] % 7 != 0)
-		{
-			std::cout << "Prvi prost broj je:" << arr[i] << std::endl;
-			std::cin.get();
-			return 0;	
-		}
-		
+		byte result;
+		result._value = _value + other._value;
+		return result;
+	}
+
+	byte operator++(int)
+	{
+		byte result;
+		result._value = ++_value;
+		return result;
 	}
 
 
-	std::cin.get();
+
+	friend std::ostream &operator<<(std::ostream &out, const byte &current)
+	{
+		out << current._value;
+		return out;
+	}
+
+	friend std::istream &operator>>(std::istream &in, byte &current)
+	{
+		int temp;
+		in >> temp;
+
+		if (temp > std::numeric_limits<char>::max())
+		{
+			std::cout << "[ERROR] char numeric limit reached!" << std::endl;
+		}
+		else
+		{
+			current._value = temp;
+			std::cin.ignore(1000, '\n');
+			std::cin.clear();
+			return in;
+		}
+	}
+	
+private:
+	char _value;
+};
+
+int main()
+{
+	byte a, b;
+	std::cin >> a >> b;
+	byte c = a + b;
+	std::cout << c++ << std::endl;
+	system("PAUSE");
 }
 
